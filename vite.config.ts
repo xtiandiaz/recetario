95def710e3a9af3de0b7e50e84eb6b210d8807ab
Cristian Díaz <xtndiaz@gmail.com>
2025-06-18 14:49:09 -0500

    Add submodules, symbolic links and reorganize json files

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,26 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueDevTools(),
+    vue(), 
+    vueDevTools()
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@design-tokens': fileURLToPath(new URL('./src/assets/design-tokens', import.meta.url)),
+      '@vueties': fileURLToPath(new URL('./src/vueties', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.names && assetInfo.names.find((name) => /\.woff2$/.test(name))) {
+            return 'assets/fonts/[name].[ext]'
+          }
+          return 'assets/[name]-[hash].[ext]'
+        },
+      },
     },
   },
 })
