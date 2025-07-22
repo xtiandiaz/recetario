226c2d044358ed20f2518032c057aad6aee2bfcb
Cristian Díaz <xtndiaz@gmail.com>
2025-07-22 15:38:08 +0300

    Update inventory, recipe and vueties

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
-
-app
+createApp(App)
   .use(createPinia())
   .use(router)
   .mount('#app')
